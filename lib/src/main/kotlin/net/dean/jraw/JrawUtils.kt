93dd93b19c8409f218c95ebdfa7500e33edc6c09
mfgm2 <repom2@airmail.cc>
2020-08-30 20:05:40 +0300

    Revert "GalleryData adapter"
    This reverts commit df64b2cc36e7cf711db36208a08a7e120a745d63.

--- lib/src/main/kotlin/net/dean/jraw/JrawUtils.kt
+++ lib/src/main/kotlin/net/dean/jraw/JrawUtils.kt
@@ -40,7 +40,6 @@ object JrawUtils {
         .add(VoteDirection::class.java, VoteDirectionAdapter())
         .add(RedditExceptionStubAdapterFactory())
         .add(LiveWebSocketUpdateAdapterFactory())
-        .add(GalleryData::class.java, GalleryDataAdapter())
         .build()
 
     /** Creates a JsonAdapter for an implied type. Convenience function using reified generics. */
